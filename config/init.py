1366c0d2205df6b54e5b54f856465849518ae768
cnunezf <camilo.nunez@cnf.cl>
2024-02-04 18:52:28 -0300

    added baseconfig function

--- config/init.py
+++ config/init.py
@@ -29,3 +29,15 @@ def default_config():
     _C.MODEL.HEAD = OmegaConf.structured(HEAD)
     
     return _C
+
+## CONFIG FOR TRAIN_IN1K.py
+def create_train_in1k_config(args):
+    model_backbone_conf = OmegaConf.load(args.cfg_model_backbone)
+    model_neck_conf = OmegaConf.load(args.cfg_model_neck)
+    model_head_conf = OmegaConf.load(args.cfg_model_head)
+    
+    base_config = default_config()
+    
+    base_config.MODEL = OmegaConf.merge(base_config.MODEL, model_backbone_conf, model_neck_conf, model_head_conf)
+
+    return base_config
