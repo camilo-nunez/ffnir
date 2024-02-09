9da23496b15764ac851db5207f964f1b00eb8240
cnunezf <camilo.nunez@cnf.cl>
2024-02-08 22:31:11 -0300

    updated citation comments; new minihead model to avoid repeat layers; updated OP gem; new layers for minihead def: bn1d, dp(p=0.6), l2-norm, and another fc layer after l2-norm.

--- model/lgffem.py
+++ model/lgffem.py
@@ -8,8 +8,12 @@ from model.neck import LGFFN
 from collections import OrderedDict
 from omegaconf.dictconfig import DictConfig
 
-## https://github.com/filipradenovic/cnnimageretrieval-pytorch/blob/master/cirtorch/layers/pooling.py
 class GeM(nn.Module):
+    ### Original code extracted from:
+    # Title: CNN Image Retrieval in PyTorch: Training and evaluating CNNs for Image Retrieval in PyTorch
+    # Retrieved Date: feb-2024
+    # Availability: https://github.com/filipradenovic/cnnimageretrieval-pytorch/blob/master/cirtorch/layers/pooling.py
+    ###
     def __init__(self,
                  p: float,
                  eps: float):
@@ -21,8 +25,31 @@ class GeM(nn.Module):
         return self.gem(x, p=self.p, eps=self.eps)
 
     def gem(self, x, p, eps):
-        return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1./p)
-        # return F.lp_pool2d(F.threshold(x, eps, eps), p, (x.size(-2), x.size(-1))) # alternative
+        return F.adaptive_avg_pool2d(x.clamp(min=eps).pow(p), 1).pow(1./p)
+
+class MiniHead(nn.Module):
+    def __init__(self, 
+                 base_config: DictConfig):
+        super().__init__()
+        
+        self._BASECONFIG = base_config
+        
+        self.gem = GeM(p=self._BASECONFIG.MODEL.HEAD.P, eps=self._BASECONFIG.MODEL.HEAD.EPS)
+        self.bn = nn.BatchNorm1d(self._BASECONFIG.MODEL.NECK.NUM_CHANNELS)
+        self.do = nn.Dropout(p=0.6)
+        self.fc = nn.Linear(self._BASECONFIG.MODEL.NECK.NUM_CHANNELS, self._BASECONFIG.MODEL.NECK.NUM_CHANNELS)
+        self.fc_norm = nn.Linear(self._BASECONFIG.MODEL.NECK.NUM_CHANNELS, self._BASECONFIG.MODEL.NECK.NUM_CHANNELS)
+        
+    def forward(self, x):
+        
+        x = self.gem(x).squeeze(0,2,3)
+        x = self.bn(x)
+        x = self.do(x)
+        x = self.fc(x)
+        x = F.normalize(x, p=2, dim=-1) ## l2-norm
+        x = self.fc_norm(x)
+
+        return x
         
 class GeMHead(nn.Module):
     def __init__(self, 
@@ -32,36 +59,20 @@ class GeMHead(nn.Module):
         
         self._BASECONFIG = base_config
         
-        self.h1 = GeM(p=self._BASECONFIG.MODEL.HEAD.P, eps=self._BASECONFIG.MODEL.HEAD.EPS)
-        self.h2 = GeM(p=self._BASECONFIG.MODEL.HEAD.P, eps=self._BASECONFIG.MODEL.HEAD.EPS)
-        self.h3 = GeM(p=self._BASECONFIG.MODEL.HEAD.P, eps=self._BASECONFIG.MODEL.HEAD.EPS)
-        self.h4 = GeM(p=self._BASECONFIG.MODEL.HEAD.P, eps=self._BASECONFIG.MODEL.HEAD.EPS)
-        
-        self.h1_fc = nn.Linear(self._BASECONFIG.MODEL.NECK.NUM_CHANNELS, self._BASECONFIG.MODEL.NECK.NUM_CHANNELS)
-        self.h2_fc = nn.Linear(self._BASECONFIG.MODEL.NECK.NUM_CHANNELS, self._BASECONFIG.MODEL.NECK.NUM_CHANNELS)
-        self.h3_fc = nn.Linear(self._BASECONFIG.MODEL.NECK.NUM_CHANNELS, self._BASECONFIG.MODEL.NECK.NUM_CHANNELS)
-        self.h4_fc = nn.Linear(self._BASECONFIG.MODEL.NECK.NUM_CHANNELS, self._BASECONFIG.MODEL.NECK.NUM_CHANNELS)
+        self.h1 = MiniHead(self._BASECONFIG)
+        self.h2 = MiniHead(self._BASECONFIG)
+        self.h3 = MiniHead(self._BASECONFIG)
+        self.h4 = MiniHead(self._BASECONFIG)
     
     def forward(self, x):
         
         if isinstance(x, OrderedDict): x = list(x.values())
         p1, p2, p3, p4 = x
         
-        h1 = self.h1(p1).squeeze(0,2,3)
-        h1 = F.normalize(h1, p=2, dim=-1)
-        h1 = self.h1_fc(h1)
-        
-        h2 = self.h2(p2).squeeze(0,2,3)
-        h2 = F.normalize(h2, p=2, dim=-1)
-        h2 = self.h2_fc(h2)
-        
-        h3 = self.h3(p3).squeeze(0,2,3)
-        h3 = F.normalize(h3, p=2, dim=-1)
-        h3 = self.h3_fc(h3)
-        
-        h4 = self.h4(p4).squeeze(0,2,3)
-        h4 = F.normalize(h4, p=2, dim=-1)
-        h4 = self.h4_fc(h4)
+        h1 = self.h1(p1)
+        h2 = self.h2(p2)
+        h3 = self.h3(p3)
+        h4 = self.h4(p4)
 
         return torch.cat([h1,h2,h3,h4], dim=1)
         
